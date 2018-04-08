269bb9b40ddb06ce18202141065ef86ab917c3a2
Abhishek Dhameja <dhameja.abhishek@gmail.com>
2018-04-07 18:23:40 -0700

    handled reserved ip error

--- src/getIPWhois.py
+++ src/getIPWhois.py
@@ -1,9 +1,12 @@
-from ipwhois import IPWhois
+import ipwhois as ipw
 
 def getIPWhois(ip):
-    obj=IPWhois(ip)
+    try:
+        obj=ipw.IPWhois(ip)
+    except ipw.ipwhois.IPDefinedError:
+        return
     result=obj.lookup_rws()
     return result['nets'][0]['name'],result['nets'][0]['description']
 
 
-##print getIPWhois('74.125.227.206')
+#print getIPWhois('172.17.25.25')
